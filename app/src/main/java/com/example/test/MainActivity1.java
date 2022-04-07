9cb82398717e880beb15b861553aaa065d5a56aa
dochu <dochu8@gmail.com>
2022-04-07 12:43:29 +0700

    Nothing special

---
+++ app/src/main/java/com/example/test/MainActivity1.java
@@ -0,0 +1,31 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity1 extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mshowCount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+        mshowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view){
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    public void countUp(View view){
+        mCount++;
+        if (mshowCount != null) {
+            mshowCount.setText(Integer.toString(mCount));
+        }
+    }
+}
